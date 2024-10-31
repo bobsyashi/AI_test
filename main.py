17723b91c3d33f97fe0cbb786bbd3ea8a6995a81
bobsyashi <113643287+bobsyashi@users.noreply.github.com>
2024-10-31 15:19:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import keras
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 data = pd.read_csv['cancer.csv']
@@ -12,3 +12,15 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 print(f"x_train: {x_train.shape}")
 print(f"y_train: {y_train.shape}")
+
+model = tf.keras.models.Sequential()
+
+model.add(tf.keras.layers.Dense(128, input_shape=x_train.shape[1:], activation='sigmoid'))
+model.add(tf.keras.layers.Dense(128, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(1,  activation='sigmoid'))
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=1000)
+
+model.evaluate(x_test, y_test)
